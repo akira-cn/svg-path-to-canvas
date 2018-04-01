d865cbc3b19d5b9e9a50f44e38854aa81e02d1ea
akira-cn <akira.cn@gmail.com>
2018-04-02 00:22:04 +0800

    add cache to point-at-length

--- src/platform/index.js
+++ src/platform/index.js
@@ -2,13 +2,24 @@ import pointInPath from './point-in-path'
 
 const getPoints = require('point-at-length')
 
-export function getPointAtLength(d, len) {
+const cacheMap = new Map()
+
+function get(d) {
+  if(cacheMap.has(d)) {
+    return cacheMap.get(d)
+  }
   const points = getPoints(d)
+  cacheMap.set(d, points)
+  return points
+}
+
+export function getPointAtLength(d, len) {
+  const points = get(d)
   return points.at(len)
 }
 
 export function getTotalLength(d) {
-  const points = getPoints(d)
+  const points = get(d)
   return points.length()
 }
 
